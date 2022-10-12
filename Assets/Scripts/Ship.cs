35a5e5597d2e76c73d2fb888e7748d02b6cabde4
AwesomenessPrimal <brendanfisher88@gmail.com>
2022-10-12 16:26:29 -0400

    Finished Prototype 0

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,13 +5,18 @@ using UnityEngine.InputSystem;
 
 public class Ship : MonoBehaviour
 {
+    [Range(1, 100)]
+    [SerializeField] private int speed = 15, turning = 40;
     private Rigidbody rb;
     private float movementX, movementY;
+    private Vector2 direction;
+    private Vector3 turnVector;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        turnVector = new Vector3(0, turning*10, 0);
     }
 
     void OnMove(InputValue movementValue)
@@ -20,16 +25,20 @@ public class Ship : MonoBehaviour
 
         movementX = movementVector.x*-100;
         movementY = movementVector.y;
+
+        direction = movementVector.normalized;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(0.0f, 0.0f, movementY);
-        Vector3 rotation = new Vector3(0, 0, movementX);
-        Quaternion deltaRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        //Quaternion deltaRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-        rb.AddForce(movement);
+        //rb.AddForce(movement);
+        //rb.MoveRotation(rb.rotation * deltaRotation);
+
+        Quaternion deltaRotation = Quaternion.Euler(direction.x * turnVector * Time.deltaTime);
         rb.MoveRotation(rb.rotation * deltaRotation);
+        rb.MovePosition(rb.position + transform.forward * direction.y * speed * Time.deltaTime);
     }
 }
