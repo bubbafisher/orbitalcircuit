131099a7ce796acc3f9eb10556ca234f19a239dd
AwesomenessPrimal <brendanfisher88@gmail.com>
2022-10-14 14:04:14 -0400

    Moved controls to Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : Ship
+{
+    private float movementX, movementY;
+    private Vector2 direction;
+    private Vector3 turnVector;
+
+    private void Start()
+    {
+        base.rb = GetComponent<Rigidbody>();
+        turnVector = new Vector3(0, turning * 10, 0);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x * -100;
+        movementY = movementVector.y;
+
+        direction = movementVector.normalized;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(direction.x * turnVector * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+        rb.MovePosition(rb.position + transform.forward * direction.y * base.speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,39 +6,8 @@ using UnityEngine.InputSystem;
 public class Ship : MonoBehaviour
 {
     [Range(1, 100)]
-    [SerializeField] private int speed = 15, turning = 40;
-    private Rigidbody rb;
-    private float movementX, movementY;
-    private Vector2 direction;
-    private Vector3 turnVector;
+    [SerializeField] protected int speed = 15, turning = 40;
+    protected Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        turnVector = new Vector3(0, turning*10, 0);
-    }
 
-    void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x*-100;
-        movementY = movementVector.y;
-
-        direction = movementVector.normalized;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //Quaternion deltaRotation = Quaternion.Euler(rotation * Time.fixedDeltaTime);
-
-        //rb.AddForce(movement);
-        //rb.MoveRotation(rb.rotation * deltaRotation);
-
-        Quaternion deltaRotation = Quaternion.Euler(direction.x * turnVector * Time.deltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-        rb.MovePosition(rb.position + transform.forward * direction.y * speed * Time.deltaTime);
-    }
 }
