3a0830fa81a7b3811754eaf040139b4a9cc29efa
AwesomenessPrimal <brendanfisher88@gmail.com>
2022-10-26 09:53:22 -0400

    Remade player prefab with new ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum ShipType
+{
+    Type1 = 0, Type2 = 1, Type3 = 2, Type4 = 3, Type5 = 4, Type6 = 5
+}
+
 public class Ship : MonoBehaviour
 {
     [Range(1, 100)]
     [SerializeField] protected int speed = 15, handling = 40, weight, health;
-    [SerializeReference] protected Color color;
+    [SerializeField] protected ShipType shipType;
     protected int gridPostion;
     protected float distanceToCelesitalBody;
     protected Rigidbody rb;
-
-
 }
