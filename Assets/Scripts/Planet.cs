0c80fe9f51a1b907ed79db60b23e3e3145eb7e39
AwesomenessPrimal <brendanfisher88@gmail.com>
2022-10-29 10:20:03 -0400

    Added Gravity!
    The ship is now pulled towards the planet. The gravity itself needs to be stronger, but that can be adjusted

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -9,17 +9,93 @@ public enum PlanetType
 
 public class Planet : MonoBehaviour
 {
-    public float mass, volume;
+    private double mass;
+    private float diameter, volume;
     public PlanetType type;
+    private Transform tf;
+    private bool pulling = false;
+    private GameObject ship;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tf = gameObject.transform;
+        switch(type)
+        {
+            case PlanetType.Planet:
+                diameter = 65;
+                mass = 1.443e8;
+                volume = 20;
+                break;
+            case PlanetType.Star:
+                diameter = 200;
+                mass = 1.072e9;
+                volume = 20;
+                break;
+            case PlanetType.Moon:
+                diameter = 20;
+                mass = 2.5467e6;
+                volume = 20;
+                break;
+            case PlanetType.BlackHole:
+                diameter = 0;
+                mass = 0;
+                volume = 0;
+                break;
+        }
+        //Set size of planet if not already set
+        tf.localScale = new Vector3(diameter, diameter, diameter);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        //Check to see if object is ship
+        if(other.CompareTag("Player"))
+        {
+            ship = other.gameObject;
+            pulling = true;
+            print("Gravity!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Check to see if object is ship
+        if (other.gameObject == ship)
+        {
+            ship = null;
+            pulling = false;
+            print("No gravity.");
+        }
+    }
+    public Vector3 pull()
+    {
+        //Calculate gravity
+        float pull = calculateGravity();
+        //Calculate heading of distance
+        Vector3 heading = this.gameObject.transform.position - ship.transform.position;
+        //Calculate pull vector
+        return pull * (heading/heading.magnitude);
+    }
+
+    private float calculateGravity()
+    {
+        //Get variables for gravity formula
+        const float G = (float)6.67e-11; //G
+        float m1 = ship.GetComponent<Rigidbody>().mass; //Ship
+        float m2 = (float)mass; //Planetary body
+        float r = Vector3.Distance(this.gameObject.transform.position, ship.transform.position); //Distance
+        r *= r; //r^2
+        //Plug everything in
+        print("F =" + G + "(" + m1 + " * " + m2 + ") / " + r+"\nF = " + (G * m1 * m2 / r));
+        return G * m1 * m2 / r;
+    }
+
+    private void FixedUpdate()
+    {
+        if (pulling)
+        {
+            ship.GetComponent<Rigidbody>().AddForce(pull(), ForceMode.Impulse); //Pull on ship
+        }
     }
 }
